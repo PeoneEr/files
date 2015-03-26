db2e1baffc25747438cdaeef491b43afedfd610c
PeoneEr <peoneerko@yandex.ru>
2015-03-26 09:41:26 +0600

    accepted formats

--- lib/scan_file.rb
+++ lib/scan_file.rb
@@ -1,9 +1,12 @@
 class ScanFile
   def scan(file_name)
+    accepted_formats = ['.avi', '.mp4', '.mkv']
     if File.ftype(file_name) == 'file'
       f_name = file_name.split('/').last
-      dir_name = file_name.split('/')[-2]
-      Film.delay.create(title: f_name, dir: dir_name)
+      if accepted_formats.include? File.extname(f_name)
+        dir_name = file_name.split('/')[-2]
+        Film.delay.create(title: f_name, dir: dir_name)
+      end
     elsif File.ftype(file_name) == 'directory'
       Dir[file_name + "/*"].each { |f| scan(f)  }
     end
