744f317e86a3a2f689431feaa447edf7c1e56749
PeoneEr <peoneerko@gmail.com>
2015-03-31 22:54:24 +0700

    вернул форматы

--- lib/scan_file.rb
+++ lib/scan_file.rb
@@ -3,10 +3,10 @@ class ScanFile
     accepted_formats = ['.avi', '.mp4', '.mkv']
     if File.ftype(file_name) == 'file'
       f_name = file_name.split('/').last
-      #if accepted_formats.include? File.extname(f_name)
-      dir_name = file_name.split('/')[-2]
-      Film.delay.create(title: f_name, dir: dir_name)
-      #end
+      if accepted_formats.include? File.extname(f_name)
+        dir_name = file_name.split('/')[-2]
+        Film.delay.create(title: f_name, dir: dir_name)
+      end
     elsif File.ftype(file_name) == 'directory'
       Dir[file_name + "/*"].each { |f| scan(f)  }
     end
